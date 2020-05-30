10758a7976e70a0d1746ac0b02e1e3e8e7094c42
nikhilbhide <nikhilbhide15@gmail.com>
2020-05-30 19:07:16 +0530

    implemented cat and dog detector using CNN with pretrained VGG-16 network

---
+++ CNN/cat_n_dog_detector/cnn_pretrained_vgg16.py
@@ -0,0 +1,119 @@
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+
+# dimensions of our images.
+img_width, img_height = 512, 512
+
+top_model_weights_path = 'bottleneck_fc_model.h5'
+train_data_dir = 'dataset/train'
+validation_data_dir = 'dataset/validation'
+nb_train_samples = 2000
+nb_validation_samples = 800
+epochs = 100
+batch_size = 16
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary',
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open('bottleneck_features_train.npy', 'wb'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary',
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'wb'),
+            bottleneck_features_validation)
+    
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+    # summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def train_top_model():
+    train_data = np.load(open('bottleneck_features_train.npy','rb'))
+    train_labels = np.array(
+        [0] * int(nb_train_samples / 2) + [1] * int(nb_train_samples / 2))
+
+    validation_data = np.load(open('bottleneck_features_validation.npy','rb'))
+    validation_labels = np.array(
+        [0] * int(nb_validation_samples / 2) + [1] * int(nb_validation_samples / 2))
+
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy', metrics=['accuracy'])
+
+    print(train_data)
+    print(train_labels)
+    print(validation_data)
+    print(validation_labels)
+    history=  model.fit(train_data, train_labels,
+              epochs=epochs,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels)
+              ,verbose = 1,
+              workers=12,
+              use_multiprocessing=True)
+    
+    summarize_diagnostics(history)
+    model.save_weights(top_model_weights_path)
+
+
+
+save_bottlebeck_features()
+train_top_model()
+
+new_model = load_model('bottleneck_fc_model.h5')
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/test/1.jpg',target_size=(150,150))
+test_image=image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+result = new_model.predict(test_image)
+print(result)
+label = new_model.predict_classes(test_image)
+print(label)
+prob = new_model.predict_proba(test_image)
+print(prob)
